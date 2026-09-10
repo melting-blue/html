67043b25b087abac05dadebd303666ec77a39719
melting-blue <103924568+melting-blue@users.noreply.github.com>
2026-09-10 13:08:14 +0900

    Connect Work Archive to Supabase

--- work-archive/config.js
+++ work-archive/config.js
@@ -1,4 +1,4 @@
 window.WORK_ARCHIVE_CONFIG = {
   supabaseUrl: 'https://rkcmdpbrfsewkifskkkq.supabase.co',
-  supabaseAnonKey: ''
+  supabaseAnonKey: 'sb_publishable_d_xuZvGqh1yG9OfVncEiow_W64g_Iai'
 };
